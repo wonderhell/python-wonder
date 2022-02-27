5151a896fb5f2ece23db407904f1f039f2f77604
wonderhell <100469998+wonderhell@users.noreply.github.com>
2022-02-27 16:55:30 +0530

    Add files via upload

--- extension.py
+++ extension.py
@@ -1,1 +1,17 @@
+import os
 
+
+# this will return a tuple of root and extension
+split_tup = os.path.splitext('hello.python')
+print(split_tup)
+
+# extract the file name and extension
+file_name = split_tup[0]
+file_extension = split_tup[1]
+
+if file_extension == ".py":
+    file_extension = "python"
+
+
+print("File Name: ", file_name)
+print("File Extension: ", file_extension)
