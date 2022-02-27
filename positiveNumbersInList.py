57afb100081bb8be848114565c3abafb906599ee
wonderhell <100469998+wonderhell@users.noreply.github.com>
2022-02-27 17:08:32 +0530

    Add files via upload

---
+++ positiveNumbersInList.py
@@ -0,0 +1,20 @@
+# Python program to print positive Numbers in a List
+
+# list of numbers
+list1 = [12, -7, 5, 64, -14]
+
+# iterating each number in list
+for num in list1:
+	
+	# checking condition
+	if num >= 0:
+	    print()
+list2 = [12, 14 , -95 , 3]	
+
+#iterating each number in list
+for num in list2:
+
+	# checking condition
+	if num >= 0:
+		print()
+
